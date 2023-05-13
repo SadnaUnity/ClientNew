bb36c7b8df96322079a96de0f398e65e079f41c8
Inbal Nitzani <inbalni@mta.ac.il>
2023-05-13 16:53:12 +0300

    room background

--- Assets/Scripts/RoomScript.cs
+++ Assets/Scripts/RoomScript.cs
@@ -8,10 +8,20 @@ public class RoomScript : MonoBehaviour
     
     private HttpRequest httpRequest;
     private Player playerData;
+    [SerializeField] private GameObject background;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Attach a Sprite Renderer component to the background game object
+        SpriteRenderer backroundspriteRenderer = background.AddComponent<SpriteRenderer>();
+
+        // Set the background image as the sprite for the Sprite Renderer
+        backroundspriteRenderer.sprite = Resources.Load<Sprite>("Images/backrounnds/vecteezy_big-shuttle-window-on-spaceship-with-view-of-other-planets_6951237"); 
+
+        // Set the sorting layer of the background object to a lower value
+        backroundspriteRenderer.sortingLayerName = "Background";
+        backroundspriteRenderer.sortingOrder = -1;
         httpRequest = new HttpRequest();
         playerData = PlayerDataManager.PlayerData;
         List<KeyValuePair<string, object>> queryParams = new List<KeyValuePair<string, object>>
