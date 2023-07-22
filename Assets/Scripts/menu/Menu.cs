67ab76625edd49644a9cb942b0702225d0434147
Inbal Nitzani <inbalni@mta.ac.il>
2023-07-22 23:17:32 +0300

    notifications and UI

--- Assets/Scripts/menu/Menu.cs
+++ Assets/Scripts/menu/Menu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,13 +8,24 @@ public class Menu : MonoBehaviour
 {
     [SerializeField] private Button menuBtn;
     [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameObject notification;
     private RectTransform btnRectTransform;
+    private RectTransform notificationBtnRectTransform;
+
     private bool showMenu;
+    private HttpRequest httpRequest;
+    private Player playerData;
     public void Start()
     {
+        httpRequest = new HttpRequest();
+        playerData = PlayerDataManager.PlayerData;
         btnRectTransform = menuBtn.GetComponent<RectTransform>();
+        notificationBtnRectTransform = notification.GetComponent<RectTransform>();
         showMenu = true;
         menuPanel.SetActive(showMenu);
+        notification.SetActive(false);
+        InvokeRepeating("UnseenNotification", 0f, 60f);
+
     }
     public void ShowHideMenu()
     {
@@ -21,14 +33,41 @@ public class Menu : MonoBehaviour
         if (showMenu)
         {
             menuPanel.SetActive(true);
-            btnRectTransform.anchoredPosition = new Vector2(-875, 500);
+            btnRectTransform.anchoredPosition = new Vector2(-900, 480);
+            notificationBtnRectTransform.anchoredPosition = new Vector2(-875, 440);
+
         }
         else
         {
             menuPanel.SetActive(false);
-            btnRectTransform.anchoredPosition = new Vector2(-1175, 500);
+            btnRectTransform.anchoredPosition = new Vector2(-1200, 480);
+            notificationBtnRectTransform.anchoredPosition = new Vector2(-1175, 440);
+
         }
         
         
+    }
+
+    private void UnseenNotification()
+    {
+        List<KeyValuePair<string, object>> queryParams = new List<KeyValuePair<string, object>>
+        {
+            new("managerId", playerData.GetUserId())
+        };
+        var res = httpRequest.SendDataToServer(queryParams, "", "/waitingJoinRoomRequests", "GET");
+        if (res.Item1 == 200)
+        {
+            RequstesDTO requstesDto = JsonConvert.DeserializeObject<RequstesDTO>(res.Item2);
+            
+            if (requstesDto.joinRoomRequests != null)
+            {
+                notification.SetActive(true);
+            }
+            else
+            {
+                notification.SetActive(false);
+            }
+
+        }
     }
 }
